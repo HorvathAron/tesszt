1712993c813a5182d99b8fea510499b2693250d3
HorvathAron <horvatharon9431@gmail.com>
2023-09-24 17:34:26 +0200

    ADD FASZSÁG

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@ concept boolean-testable =
     __boolean_testable_impl<B> &&
 b) {
         { !std::forward<B>(b) } -> __boolean_testable_impl;
-    };
-
+    };űűwfegoiwirobkőqel,vbn orgjá  wfpqöj
+    r jml;WFÉREŰBN SV,ÉLAFÉ ERNQBW SLLFPÚKO SVDLM,ÁÉFVQE GNKAV,láDEIPQBN VLÁGLDŰE   NPB L,DAPVNEJSLBL,ÉAVN LWLÉDV   JNEQBLFLDNQEOJ 
     // blabla
