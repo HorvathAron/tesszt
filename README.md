2f0533132aca8b6649159207316433fddae40843
HorvathAron <horvatharon9431@gmail.com>
2023-09-24 17:32:26 +0200

    hiba

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tesszt
 ki fogom törölni
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 template< class B >
 concept boolean-testable =                       
     __boolean_testable_impl<B> &&
-    requires (B&& b) {
+b) {
         { !std::forward<B>(b) } -> __boolean_testable_impl;
     };
+
+    // blabla
